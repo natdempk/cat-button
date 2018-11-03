f0e216061346e5cf57724d05e23b3886dc7abc88
Nat Dempkowski <natdempk@gmail.com>
2018-11-03 13:52:57 -0400

    Fix build script

--- package.fish
+++ package.fish
@@ -1,3 +1,7 @@
 #!/usr/bin/env fish
+set basedir $PWD
+echo $basedir
 rm cat_button.zip
-zip -r9 cat_button.zip cat_button_venv/lib/python3.6/site-packages/ cat_button.py
+zip -9 $basedir/cat_button.zip cat_button.py
+cd cat_button_venv/lib/python3.6/site-packages 
+zip -gr9 $basedir/cat_button.zip . 
