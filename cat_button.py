541cb737caf91345081b46a7be0d477e79244549
Nat Dempkowski <natdempk@gmail.com>
2018-11-03 13:15:02 -0400

    Fix bugs, add random emoji

--- cat_button.py
+++ cat_button.py
@@ -1,17 +1,24 @@
 import os
+import random
 from slackclient import SlackClient
 
 TEST_CHANNEL = "C7M386WN8"
 HOLLAND_CHANNEL = "G8TU9CA92" 
 
-def hander(event, context):
+MILOS = [":milo:", ":milo_glare:"]
+LUNAS = [":luna_wat:", ":luna_derp:", ":luna_cat:"]
+
+def handler(event, context):
     slack_token = os.environ["SLACK_API_TOKEN"]
     sc = SlackClient(slack_token)
 
-    sc.api_call(
+    milo = random.choice(MILOS)
+    luna = random.choice(LUNAS)
+
+    response = sc.api_call(
         "chat.postMessage",
         channel=TEST_CHANNEL,
-        text=":milo: :luna: The cats have been fed!",
+        text=f"{milo} {luna} The cats have been fed!",
         as_user=False,
         username="CatBot"
     )
@@ -25,3 +32,7 @@ def hander(event, context):
                 'statusCode':500,
                 'body': json.dumps(response)
         }
+
+
+if __name__ == "__main__":
+    handler("test", "test")
